92fde0b7444ad6c277fa7885bdbb2b241ee39c6d
Shavkat Metekov <metekov.shavkat@gmail.com>
2020-01-30 20:21:35 -0600

    big changes

--- script.js
+++ script.js
@@ -28,6 +28,20 @@ $(document).ready(function() {
         
         });
     }
-    
+    function search_restaurant() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('restaurants'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="in-line-block";                  
+        } 
+    } 
+}
+
     navigator.geolocation.getCurrentPosition(giveLocation);
 });
